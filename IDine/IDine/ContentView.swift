adfe4f3327d10c66894fc6ecd13a927623e1c886
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-24 17:17:05 +0530

    Menu Data loaded from json file and shown on the screen. Models are confirming to Identifiable so that can use them for showing on UI.

--- IDine/IDine/ContentView.swift
+++ IDine/IDine/ContentView.swift
@@ -9,12 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
         NavigationView {
             List {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
+                ForEach(menu) { section in
+                    Text(section.name)
+                }
             }.navigationBarTitle("Menu")
         }
     }
