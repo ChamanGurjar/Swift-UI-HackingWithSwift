eaafe069c9b1a042115f807e423e6c56f999fa28
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-24 16:47:43 +0530

    Project Created

---
+++ IDine/IDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IDine
+//
+//  Created by Chaman Gurjar on 24/10/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
