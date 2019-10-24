5e1a6ff7bc18443ab7970b1decb8725d27458aba
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-24 17:03:18 +0530

    List view with 4 rows created.

--- IDine/IDine/ContentView.swift
+++ IDine/IDine/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        List {
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+        }
     }
 }
 
