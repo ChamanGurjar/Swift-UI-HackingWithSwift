b6d2208007bb008c67bb861ee49c6cb804621d58
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-24 17:31:05 +0530

    Project Structure introduced.

---
+++ IDine/IDine/Views/MenuRow.swift
@@ -0,0 +1,21 @@
+//
+//  MenuRowDesign.swift
+//  IDine
+//
+//  Created by Chaman Gurjar on 24/10/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuRow: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct MenuRow_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuRow()
+    }
+}
