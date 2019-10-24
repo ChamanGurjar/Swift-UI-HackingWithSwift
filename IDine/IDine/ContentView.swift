4038b7125ba0fa4367d57aa2639c44e47ac7994c
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-24 17:24:05 +0530

    Header added for each section

--- IDine/IDine/ContentView.swift
+++ IDine/IDine/ContentView.swift
@@ -16,7 +16,13 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(menu) { section in
-                    Text(section.name)
+                    Section(header: Text(section.name)) {
+                         
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                        
+                    }
                 }
             }.navigationBarTitle("Menu")
         }
