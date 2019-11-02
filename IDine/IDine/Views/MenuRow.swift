a095539aca56606c85a639840fe774d047421b94
Chaman Gurjar <chamangujjar162@gmail.com>
2019-11-02 21:44:15 +0530

    Restrictions view in circle shape added. Different modifier for TextView Added like font, fontWeight, padding, background, clipShape, foregroundColor etc.

--- IDine/IDine/Views/MenuRow.swift
+++ IDine/IDine/Views/MenuRow.swift
@@ -9,16 +9,33 @@
 import SwiftUI
 
 struct MenuRow: View {
-    
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     var item: MenuItem
     
     var body: some View {
         HStack {
-            Image(item.thumbnailImage).clipShape(Circle())
+            Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2.0))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("RS \(item.price)")
+            }.layoutPriority(1)
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) {
+                restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(self.colors[restriction])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+                
             }
             
         }
