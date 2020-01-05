bd9816a970743cf25604397e43d14a73639dc633
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-05 17:51:27 +0530

    Initial Commit - WeSplit Project Setup

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by ChamanGurjar on 05/01/20.
+//  Copyright © 2020 ChamanGurjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
