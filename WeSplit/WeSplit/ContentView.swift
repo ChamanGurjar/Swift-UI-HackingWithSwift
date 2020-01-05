f1369b610adf3dc0090b75a9bbd1fce09b7a21f1
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-05 19:41:10 +0530

    Text Field Implemented. 2 Way binding added.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,12 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""   // Using @State you can change the property in a method in struct without using the mutating keyword.
+    
     var body: some View {
         NavigationView {
             Form {
-                Section {
-                    Text("Hello, World!")
-                }
+                TextField("Enter your name here", text: $name) // $ is used for 2 way binding here.
+                Text("Your name is : \(name)")
             }
                 // .navigationBarTitle(Text("We Split"), displayMode: .inline) // Small Size navigation bar title
                 .navigationBarTitle("We Split")
