95069896eb37f48a9f435f44e89ee441b3684695
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-26 23:05:16 +0530

    Methods to check 1. Word is not used before. 2. Word can be created using the root/shown word. 3. Word is real or not
    are added.

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -54,6 +54,30 @@ struct ContentView: View {
         fatalError("Couldn't load start.txt from bundle")
     }
     
+    private func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    private func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
