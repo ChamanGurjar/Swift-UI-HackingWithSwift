8cfc2661224c54b192f18b7d3353d90dd5c09bd7
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-26 21:37:07 +0530

    Text field to enter new word added. Already added words are shown in the list.

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -15,7 +15,18 @@ struct ContentView: View {
     @State private var newWord = ""
     
     var body: some View {
-        Text("Body")
+        NavigationView {
+            VStack {
+                TextField("Enter your word",  text: $newWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
     }
 }
 
