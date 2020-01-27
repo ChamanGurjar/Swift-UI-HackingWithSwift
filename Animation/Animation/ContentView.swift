c980d140db4b5f41709410f525eabc4bc2cf4174
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-27 17:49:27 +0530

    A gradient or lets say a ui with movable gesture implemented.

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -10,19 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 0.0
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
-            }
-        }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount = $0.translation }
+                    .onEnded {_ in self.dragAmount = .zero}     
+        )
+        
     }
 }
 
