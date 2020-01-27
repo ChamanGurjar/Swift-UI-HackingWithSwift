151fd620916e2ef3225f92c4769292400b300fe2
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-27 16:28:52 +0530

    An amazing spring animation with stiffness added.

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            withAnimation {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
                 self.animationAmount += 360
             }
         }
