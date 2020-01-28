60d09d18edc371bf93df018fa7f016de2cc46aa3
ChamanGurjar <chamangujjar162@gmail.com>
2020-01-28 18:24:02 +0530

    A button with 360 rotation is added.

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -11,29 +11,50 @@ import SwiftUI
 struct ContentView: View {
     
     let letters = Array("Chaman Gurjar")
+    let colorsArray = [Color.gray, Color.green, Color.yellow, Color.blue, Color.black]
     @State private var dragAmount = CGSize.zero
     @State private var enabled = false
     
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) { num in
-                Text(String(self.letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(self.enabled ? Color.blue : Color.red)
-                    .offset(self.dragAmount)
-                    .animation(Animation.default.delay(Double(num)/20))
+        VStack(spacing: 20) {
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count) { num in
+                    Text(String(self.letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(self.enabled ? Color.blue : Color.red)
+                        .offset(self.dragAmount)
+                        .animation(Animation.default.delay(Double(num)/20))
+                }
             }
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount = $0.translation }
+                    .onEnded { _ in
+                        self.dragAmount = CGSize.zero
+                        self.enabled.toggle()
+                }
+            )
+            
+            Button("Tap Me")
+            {
+                //                self.background(self.colorsArray.randomElement() ?? Color.red)
+                withAnimation {
+                    self.animationAmount += 360
+                }
+            }
+            .padding(50)
+            .foregroundColor(Color.white)
+                //.background(Color.red)
+                .background(self.colorsArray.randomElement() ?? Color.red)
+                .clipShape(Circle())
+                .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
         }
-        .gesture(
-            DragGesture()
-                .onChanged { self.dragAmount = $0.translation }
-                .onEnded { _ in
-                    self.dragAmount = CGSize.zero
-                    self.enabled.toggle()
-            }
-        )
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
