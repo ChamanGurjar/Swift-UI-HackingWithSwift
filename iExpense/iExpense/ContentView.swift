fb2d8e66c86010653e3a97a5955b8f50c2ce66f4
ChamanGurjar <chamangujjar162@gmail.com>
2020-02-15 20:02:55 +0530

    Showing Delete option in row of a list and on left swipe delete a row.

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,27 +8,32 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
-    var name: String
-    
-    var body: some View {
-        Button("Dismiss") {
-            self.presentationMode.wrappedValue.dismiss()
-        }
-    }
-}
+
 
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button("Show Sheet") {
-            self.showingSheet.toggle()
+        
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("\($0)")
+                }
+                .onDelete(perform: removeRows(at:))
+            }
+            
+            Button("Add Number") {
+                self.numbers.append(self.currentNumber)
+                self.currentNumber += 1
+            }
+            
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "This is the second Sheet")
-        }
+    }
+    
+    private func removeRows(at offset: IndexSet) {
+        numbers.remove(atOffsets: offset)
     }
 }
 
