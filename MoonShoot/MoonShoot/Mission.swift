030b200f741c5a6ff01d4fdc06f83091d5421939
ChamanGurjar <chamangujjar162@gmail.com>
2020-02-16 20:33:59 +0530

    Mission Struct added.

---
+++ MoonShoot/MoonShoot/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  MoonShoot
+//
+//  Created by ChamanGurjar on 16/02/20.
+//  Copyright © 2020 ChamanGurjar. All rights reserved.
+//
+
+import Foundation
+
+struct CrewRole: Codable {
+    let name: String
+    let role: String
+}
+
+struct Mission: Codable, Identifiable {
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
