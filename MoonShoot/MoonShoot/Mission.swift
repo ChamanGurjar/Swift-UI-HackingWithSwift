bf5dd17019300fb3f68ad8078ab2cc0a072a3221
ChamanGurjar <chamangujjar162@gmail.com>
2020-02-18 18:10:13 +0530

    Data captured from bundle and decoded via GENERICS

--- MoonShoot/MoonShoot/Mission.swift
+++ MoonShoot/MoonShoot/Mission.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 
-struct CrewRole: Codable {
-    let name: String
-    let role: String
-}
+
 
 struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
     let id: Int
     let launchDate: String?
     let crew: [CrewRole]
