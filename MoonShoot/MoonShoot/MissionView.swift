5efb42df6bf1855ea21cedb83171c288e91c4a53
ChamanGurjar <chamangujjar162@gmail.com>
2020-02-22 09:23:57 +0530

    MissionView - Functionality completed. All details of mission like description and list of crew shown.

--- MoonShoot/MoonShoot/MissionView.swift
+++ MoonShoot/MoonShoot/MissionView.swift
@@ -9,7 +9,14 @@
 import SwiftUI
 
 struct MissionView: View {
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let astronauts: [CrewMember]
     
     var body: some View {
         GeometryReader { geometry in
@@ -24,17 +31,54 @@ struct MissionView: View {
                     Text(self.mission.description)
                         .padding()
                     
+                    ForEach(self.astronauts, id: \.role) { crewMember in
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 83, height: 60)
+                                .clipShape(Capsule())
+                                .overlay(Capsule()
+                                    .stroke(Color.primary, lineWidth: 1))
+                            
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundColor(.secondary)
+                            }
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                    
                     Spacer(minLength: 25)
                 }
             }
         }
         .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
     }
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+        
+        self.astronauts = matches
+    }
 }
 
 struct MissionView_Previews: PreviewProvider {
     static let mission: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     static var previews: some View {
-        MissionView(mission: mission[0])
+        MissionView(mission: mission[0], astronauts: astronauts)
     }
 }
