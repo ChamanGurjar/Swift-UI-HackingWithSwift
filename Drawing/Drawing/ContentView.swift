fd7b4443e1855ab458de513f017fe741dfc5871a
ChamanGurjar <chamangujjar162@gmail.com>
2020-02-23 09:26:33 +0530

    A flower and Petals created using CGAffineTransform and even-odd fills

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,19 +8,74 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            //            path.addLine(to: CGPoint(x: 100, y: 300))  // To fix top broken line.
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+    
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
         }
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)) // OR To fix top broken line.
-        //        .stroke(Color.blue, lineWidth: 10)
-        // .fill(Color.blue)
+        
+        return path
     }
+    
+}
+
+struct ContentView: View {
+    
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+            //                .stroke(Color.red, lineWidth: 1)
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
+    }
+    
+    //    var body: some View {
+    //        Path { path in
+    //            path.move(to: CGPoint(x: 200, y: 100))
+    //            path.addLine(to: CGPoint(x: 100, y: 300))
+    //            path.addLine(to: CGPoint(x: 300, y: 300))
+    //            path.addLine(to: CGPoint(x: 200, y: 100))
+    //            //            path.addLine(to: CGPoint(x: 100, y: 300))  // To fix top broken line.
+    //        }
+    //            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)) // OR To fix top broken line.
+    //        //        .stroke(Color.blue, lineWidth: 10)
+    //        // .fill(Color.blue)
+    //    }
 }
 
 struct ContentView_Previews: PreviewProvider {
