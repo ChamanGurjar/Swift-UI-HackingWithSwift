d22fa2405c4e5e1e5896dfe53c0dc4987d02b406
ChamanGurjar <chamangujjar162@gmail.com>
2020-03-26 08:26:30 +0530

    Project Created - 'Cupcale-Corner'

---
+++ Cupcake-Corner/Cupcake-Corner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcake-Corner
+//
+//  Created by ChamanGurjar on 26/03/20.
+//  Copyright © 2020 ChamanGurjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
